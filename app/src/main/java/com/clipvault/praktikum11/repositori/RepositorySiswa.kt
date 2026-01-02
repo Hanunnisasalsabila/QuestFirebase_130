3b08168dbc790c57b164c6c202deebb673043f37
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-02 22:26:08 +0700

    tambah FirebaseRepositorySiswa sebagai implementasi repository siswa

--- app/src/main/java/com/clipvault/praktikum11/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/clipvault/praktikum11/repositori/RepositorySiswa.kt
@@ -1,8 +1,11 @@
 package com.clipvault.praktikum11.repositori
 
 import com.clipvault.praktikum11.modeldata.Siswa
-
+import com.google.firebase.firestore.FirebaseFirestore
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
 }
+class FirebaseRepositorySiswa : RepositorySiswa {
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")

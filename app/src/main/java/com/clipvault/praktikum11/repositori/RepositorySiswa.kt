bb9a1b20927a4e66b065b66bed72cd36a7fcd8fb
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-02 22:27:38 +0700

    implementasi getDataSiswa menggunakan Firebase Firestore

--- app/src/main/java/com/clipvault/praktikum11/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/clipvault/praktikum11/repositori/RepositorySiswa.kt
@@ -2,6 +2,8 @@ package com.clipvault.praktikum11.repositori
 
 import com.clipvault.praktikum11.modeldata.Siswa
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
@@ -9,3 +11,18 @@ interface RepositorySiswa {
 class FirebaseRepositorySiswa : RepositorySiswa {
     private val db = FirebaseFirestore.getInstance()
     private val collection = db.collection("siswa")
+
+    override suspend fun getDataSiswa(): List<Siswa> {
+        return try {
+            collection.get().await().documents.map { doc ->
+                Siswa(
+                    id = doc.getLong("id")?.toLong() ?: 0,
+                    nama = doc.getString("nama") ?: "",
+                    alamat = doc.getString("alamat") ?: "",
+                    telpon = doc.getString("telpon") ?: ""
+                )
+            }
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }

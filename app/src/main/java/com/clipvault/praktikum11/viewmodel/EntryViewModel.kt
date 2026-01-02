e373561bbb8330c5e9f213484c82e3043ce87277
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-02 22:39:59 +0700

    implementasi validasi input pada EntryViewModel

--- app/src/main/java/com/clipvault/praktikum11/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/clipvault/praktikum11/viewmodel/EntryViewModel.kt
@@ -4,9 +4,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.clipvault.praktikum11.modeldata.DetailSiswa
 import com.clipvault.praktikum11.modeldata.UIStateSiswa
 import com.clipvault.praktikum11.repositori.RepositorySiswa
+import com.clipvault.praktikum11.modeldata.toDataSiswa
 
 class EntryViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean
+    {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+}

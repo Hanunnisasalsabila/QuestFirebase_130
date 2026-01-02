e6076f725516d2ba92c35efa36bf686e60fd229e
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-02 22:22:17 +0700

    tambah DefaultContainerApp sebagai penyedia RepositorySiswa

--- app/src/main/java/com/clipvault/praktikum11/repositori/ContainerApp.kt
+++ app/src/main/java/com/clipvault/praktikum11/repositori/ContainerApp.kt
@@ -3,3 +3,8 @@ package com.clipvault.praktikum11.repositori
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}

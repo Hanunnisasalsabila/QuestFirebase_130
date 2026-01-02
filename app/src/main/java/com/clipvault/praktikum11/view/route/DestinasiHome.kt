92c7eca0481c880ae396cef6482066814b17b2ad
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-02 22:31:55 +0700

    tambah DestinasiHome sebagai halaman utama aplikasi

---
+++ app/src/main/java/com/clipvault/praktikum11/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.clipvault.praktikum11.view.route
+
+import com.clipvault.praktikum11.R
+import com.clipvault.praktikum11.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}

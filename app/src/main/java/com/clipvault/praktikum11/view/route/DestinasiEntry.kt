4dbd0afe305ad4736f45e2250b5137aa8aee3d00
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-02 22:31:26 +0700

    tambah DestinasiEntry untuk navigasi halaman input siswa

---
+++ app/src/main/java/com/clipvault/praktikum11/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.clipvault.praktikum11.view.route
+
+import com.clipvault.praktikum11.view.route.DestinasiNavigasi
+import com.clipvault.praktikum11.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}

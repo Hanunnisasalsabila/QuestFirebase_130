9a8a203f0e0ee6c5cf9277cd87cd157dc9bab4f9
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-02 22:53:27 +0700

    tambah HostNavigasi dan DataSiswaApp untuk pengaturan navigasi aplikasi

---
+++ app/src/main/java/com/clipvault/praktikum11/view/controllNavigasi/PetaNavigasi.kt
@@ -0,0 +1,40 @@
+package com.clipvault.praktikum11.view.controllNavigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.clipvault.praktikum11.view.EntrySiswaScreen
+import com.clipvault.praktikum11.view.HomeScreen
+import com.clipvault.praktikum11.view.route.DestinasiDetail
+import com.clipvault.praktikum11.view.route.DestinasiEntry
+import com.clipvault.praktikum11.view.route.DestinasiHome
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier:
+    Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier) {
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.
+                route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")})
+        }
+
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)
+                })
+        }
+    }
+}

932a4c18ab73e770afa135e9bf363c6140842641
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-08 22:13:37 +0700

    Mengambil idSiswa dari SavedStateHandle

--- app/src/main/java/com/clipvault/praktikum11/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/clipvault/praktikum11/viewmodel/DetailViewModel.kt
@@ -2,10 +2,14 @@
 
 package com.clipvault.praktikum11.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.clipvault.praktikum11.modeldata.Siswa
 import com.clipvault.praktikum11.repositori.RepositorySiswa
+import com.clipvault.praktikum11.view.route.DestinasiDetail
 import kotlinx.serialization.InternalSerializationApi
 
 sealed interface StatusUIDetail {
@@ -16,4 +20,11 @@ sealed interface StatusUIDetail {
 
 class DetailViewModel(savedStateHandle: SavedStateHandle,private val repositorySiswa:
 RepositorySiswa) : ViewModel() {
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
 }

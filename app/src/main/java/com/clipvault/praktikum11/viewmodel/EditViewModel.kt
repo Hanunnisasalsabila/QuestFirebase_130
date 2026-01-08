bff0aae61bc1b864cfe4102679e88f7581f4cc71
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-08 22:20:07 +0700

    engambil data siswa berdasarkan ID dan memetakan ke UIState

--- app/src/main/java/com/clipvault/praktikum11/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/clipvault/praktikum11/viewmodel/EditViewModel.kt
@@ -1,12 +1,29 @@
 package com.clipvault.praktikum11.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.clipvault.praktikum11.modeldata.UIStateSiswa
+import com.clipvault.praktikum11.modeldata.toUiStateSiswa
 import com.clipvault.praktikum11.repositori.RepositorySiswa
+import com.clipvault.praktikum11.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
 
 class EditViewModel(savedStateHandle: SavedStateHandle,private val repositorySiswa:
-    RepositorySiswa) : ViewModel() {
+RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
-
-
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
 }

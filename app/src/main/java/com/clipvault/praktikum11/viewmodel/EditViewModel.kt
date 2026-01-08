1a2381f52d2a36eb942a54395cad75f4f0f07f9b
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-08 22:21:21 +0700

    Menambahkan validasi input dan proses update data siswa

--- app/src/main/java/com/clipvault/praktikum11/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/clipvault/praktikum11/viewmodel/EditViewModel.kt
@@ -6,7 +6,9 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.clipvault.praktikum11.modeldata.DetailSiswa
 import com.clipvault.praktikum11.modeldata.UIStateSiswa
+import com.clipvault.praktikum11.modeldata.toDataSiswa
 import com.clipvault.praktikum11.modeldata.toUiStateSiswa
 import com.clipvault.praktikum11.repositori.RepositorySiswa
 import com.clipvault.praktikum11.view.route.DestinasiDetail
@@ -24,6 +26,28 @@ RepositorySiswa) : ViewModel() {
         viewModelScope.launch {
             uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
                 .toUiStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa,uiStateSiswa.detailSiswa.toDataSiswa
+                    ())
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
         }
     }
 }

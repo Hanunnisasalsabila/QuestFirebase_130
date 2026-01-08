1b428a6be3d0cf1d6abbb300dee8ebb0d8572e92
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-08 22:12:38 +0700

    Menambahkan DetailViewModel untuk fitur detail siswa

--- app/src/main/java/com/clipvault/praktikum11/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/clipvault/praktikum11/viewmodel/DetailViewModel.kt
@@ -1,11 +1,19 @@
 @file:OptIn(InternalSerializationApi::class)
 
 package com.clipvault.praktikum11.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.clipvault.praktikum11.modeldata.Siswa
+import com.clipvault.praktikum11.repositori.RepositorySiswa
 import kotlinx.serialization.InternalSerializationApi
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle,private val repositorySiswa:
+RepositorySiswa) : ViewModel() {
 }

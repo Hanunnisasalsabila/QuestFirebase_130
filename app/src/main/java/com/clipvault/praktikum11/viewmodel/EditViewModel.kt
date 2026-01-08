275b32102f847731bbf0c26b345c4ee62639438b
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-08 22:18:59 +0700

    Menambahkan EditViewModel untuk fitur edit data siswa

--- app/src/main/java/com/clipvault/praktikum11/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/clipvault/praktikum11/viewmodel/EditViewModel.kt
@@ -1,1 +1,12 @@
 package com.clipvault.praktikum11.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.clipvault.praktikum11.repositori.RepositorySiswa
+
+class EditViewModel(savedStateHandle: SavedStateHandle,private val repositorySiswa:
+    RepositorySiswa) : ViewModel() {
+
+
+
+}

b7f2fbb158bb4ea97f61a19fb1f43036bd3b4f81
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-01-08 22:11:42 +0700

    Menambahkan StatusUIDetail untuk state loading, sukses, dan error

--- app/src/main/java/com/clipvault/praktikum11/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/clipvault/praktikum11/viewmodel/DetailViewModel.kt
@@ -1,1 +1,11 @@
+@file:OptIn(InternalSerializationApi::class)
+
 package com.clipvault.praktikum11.viewmodel
+import com.clipvault.praktikum11.modeldata.Siswa
+import kotlinx.serialization.InternalSerializationApi
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
